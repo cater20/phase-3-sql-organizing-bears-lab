1422d23c9d81d5ab84dc3895c69ef7f76af664cb
chrisphine <onyangoketta@gmail.com>
2022-08-25 20:26:30 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES
+("Mr.Chocolate",11,"M","brown","cool" ,4),
+("Rowdy",12,"F","yellow","charming",0),
+("Tabitha",13,"F","green","cold",2),
+("Sergeant Brown",14,"M","black","hot",3),
+("Melissa","F",15,"white","mild",0),
+("Grinch",16,"M","purple","cold",7),
+("Wendy",17,"F","blue","hot",8),
+(NULL,18,"M","red","cold",9);
